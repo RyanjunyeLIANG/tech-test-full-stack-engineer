e8b09e3a71dacd04605096343ec4887e6eab577c
Ryan.LIANG <32515067+RyanjunyeLIANG@users.noreply.github.com>
2020-07-22 23:56:34 +1000

    Add features to fetch data from server and consume on the result display

--- client/src/components/Console.jsx
+++ client/src/components/Console.jsx
@@ -10,26 +10,58 @@ import './Console.css';
 const Console = () => {
   const [result, setResult] = useState('');
   const [text, setText] = useState('');
+
+  const url = 'http://localhost:4000';
+  const capsulesUrl = url + '/capsules';
+  const landpadUrl = url + `/landpads/${text}`;
+
+  const getCapsule = (e) => {
+    e.preventDefault();
+    fetch(capsulesUrl)
+      .then(
+        res => res.json()
+      )
+      .then(
+        jsondata => setResult(JSON.stringify(jsondata))
+      )
+      .catch(err => console.log(err));
+  }
+
+  const getLandpad = (e) => {
+    e.preventDefault();
+    console.log(landpadUrl);
+    fetch(landpadUrl)
+    .then(
+      res => res.json()
+    )
+    .then(
+      jsondata => setResult(JSON.stringify(jsondata))
+    )
+    .catch(err => setResult('404 Not found'));
+  }
   
   return (
     <div className="outer-warpper">
       <div className='display-warpper'>
-        <ResultDisplay>test 1234</ResultDisplay>
+        <ResultDisplay>{result}</ResultDisplay>
       </div>
       <div className='panel-warpper'>
         <div className='item-container' style={{width: '30%'}}>
-          <Button>Capsules</Button>
+          <Button onClick={getCapsule}>Capsules</Button>
         </div>
         <div className='item-container' style={{width: '20%'}}>
           <Rocket />
         </div>
         <div className='item-container' style={{width: '60%'}}>
+          <form onSubmit={getLandpad}>
             <InputField
               placeholder="text"
               onChange={(e) => setText(e.target.value)}
-              pattern='\w'
+              pattern='[A-Za-z0-9-_]{1,}'
+              required
             />
-            <Button>Landing Pad</Button>
+            <Button buttonType="submit">Landing Pad</Button>
+          </form>
         </div>
       </div>
     </div>
